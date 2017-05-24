b91cddcd1b0864ece55d0e9d634635b59de9b831
Gabe Becker <gmbecker@ucdavis.edu>
2017-05-24 11:46:41 -0700

    make highlightCode example work and be fast enough (turn off checkURLs)

--- R/highlightCode.R
+++ R/highlightCode.R
@@ -54,11 +54,12 @@ function(addFunctionLinks = TRUE,  checkURLs = TRUE, h)
 
       w = which(styles == "string")
       tmp = gsub('(^"|"$)', "", tokens[w])
-      e = file.exists(tmp)
+        e = file.exists(tmp)
+        e2chk = !e & nzchar(tmp) & grepl("[^.]", tmp) & grepl("[[:alnum:]]", tmp)
       if(length(tmp) > 0 && checkURLs) {
           if(!requireNamespace("RCurl"))
               stop("checking URLs requires RCurl (not found).")
-        e[!e] = sapply(tmp[!e], RCurl::url.exists) #RCurl::url.exists(tmp[!e])
+        e[e2chk] = sapply(tmp[e2chk], RCurl::url.exists) #RCurl::url.exists(tmp[!e])
       }
       i = w[e]
       if(length(i)) {
@@ -74,9 +75,10 @@ function(addFunctionLinks = TRUE,  checkURLs = TRUE, h)
 
 
 highlightCode =
-function(obj, out = NULL, addFunctionLinks = TRUE, inline = TRUE, h = htmlRenderer(addFunctionLinks),
-         css = system.file("CSS", "highlight.css", package = "CodeDepends"),
-         jsCode = system.file("JavaScript", "highlightSymbols.js", package = "CodeDepends"))
+    function(obj, out = NULL, addFunctionLinks = TRUE, checkURLs = TRUE,
+             inline = TRUE, h = htmlRenderer(addFunctionLinks, checkURLs),
+             css = system.file("CSS", "highlight.css", package = "CodeDepends"),
+             jsCode = system.file("JavaScript", "highlightSymbols.js", package = "CodeDepends"))
 {
     if(!requireNamespace("highlight") || !requireNamespace("RJSONIO"))
         stop("Unable to highlight code without the highlight and/or RJSONIO package(s)")
