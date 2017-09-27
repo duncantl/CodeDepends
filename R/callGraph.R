a1912c68c3b6c2e6f884187580bb9b9dbe9547da
Duncan Temple Lang <duncan@r-project.org>
2017-09-27 14:57:12 -0700

    class for the graph so that we can define a plot method to color the nodes based on which package they come from

--- R/callGraph.R
+++ R/callGraph.R
@@ -10,9 +10,17 @@ function(pos, syms = objects(pos))
    objs[!sapply(objs, is.null)]
 }
 
+
+setMethod("makeCallGraph", "function",
+          function(obj, all = FALSE, recursive = TRUE, ...) {
+              name = as.character(substitute(obj))
+              makeCallGraph(list(obj), all, recursive, funNames = name, ...)
+          })
+
 # Do the call graph for methods, i.e. which methods call which other methods.
 # Can we tell this.
 # Deal with global variables, i.e. non function references.
+
 
 setMethod("makeCallGraph", "character",
           function(obj, all = FALSE, recursive = TRUE, ...) {
@@ -28,7 +36,7 @@ setMethod("makeCallGraph", "character",
             }
             
             if(exists(obj, mode = "function"))
-              return(makeCallGraph(get(obj, mode = "function"), recursive = recursive, ...))
+              return(makeCallGraph(structure(list(get(obj, mode = "function")), names = obj), all = all, recursive = recursive, ...))
 
             path = search()
             if( !is.na(w <- match(obj, path)) || !is.na(w <- match(obj, gsub("^package:", "", path)))) {
@@ -43,22 +51,28 @@ setMethod("makeCallGraph", "list",
           function(obj, all = FALSE, recursive = TRUE, funNames = names(obj), packages = attr(obj, "packages"), ...) {
                # Assume all functions.
             require(graph)
-            ids = names(obj)
+            ids = funNames
             edges = lapply(obj, function(f) {
                                   calls = findGlobals(f, merge = FALSE)$functions
                                   if(all)
                                     return(calls)
                                   list(edges = match(calls[calls %in% ids], ids))
-                                })
+                              })
+
 
             if(all) {
-              ids = unlist(edges)
-              edges = lapply(edges, function(x) {
-                                          list(edges = match(x, ids))
-                                    })
+              z = lapply(edges, function(x) x[ !( x %in%  c("|", "||")) ])
+              ids = unique(c(unlist(z), ids))
+              edges = replicate(length(ids), character(), FALSE)
+              names(edges) = ids
+              edges[ names (z) ] = z
+#              edges = unlist(lapply(edges, function(x) {
+#                                             i = match(x, ids)
+#                                             list(edges = setdiff(ids[i], x))
+#                                           }))
             }
             
-            g = new("graphNEL", nodes = ids, edgeL = edges, edgemode = "directed")
+            g = new("CallGraph", nodes = ids, edgeL = edges, edgemode = "directed")
 
             if(length(packages)) {
                 nodeDataDefaults(g, "package") <- ""
@@ -68,4 +82,18 @@ setMethod("makeCallGraph", "list",
             g
           })
 
+
+setClass("CallGraph", contains = "graphNEL")
+
+setMethod("plot", "CallGraph",
+          function (x, y, ..., name = "", subGList = list(), 
+                    attrs = list(), nodeAttrs = list(), edgeAttrs = list(), 
+                    recipEdges = c("combined", "distinct"), colors= c("red", "blue"))
+          {
+              ids = nodes(g)
+              pkg = unlist(nodeData(g, , "package"))
+              nodeAttrs$color = structure(colors[factor(pkg)], names = ids)
+
+              callNextMethod(x, y, ..., name = name, subGList = subGList, attrs = attrs, nodeAttrs = nodeAttrs, edgeAttrs = edgeAttrs, recipEdges = recipEdges)
+          })
 
