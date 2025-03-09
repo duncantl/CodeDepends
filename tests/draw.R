758bda3324d6e1936ebb68839211902fb76dde6f
Duncan Temple Lang <duncan@r-project.org>
2025-03-09 16:21:32 -0700

    use require() to try to load Rgraphviz and don't run code if it is not loaded.

--- tests/draw.R
+++ tests/draw.R
@@ -1,16 +1,18 @@
 library(CodeDepends)
-library(Rgraphviz)
-sc = readScript(system.file("samples", "parallel.R", package = "CodeDepends"))
+if(require(Rgraphviz)) {
 
-g = makeVariableGraph(,sc)
-plot(g)
+    sc = readScript(system.file("samples", "parallel.R", package = "CodeDepends"))
 
-g = makeTaskGraph(,sc)
-plot(g)
+    g = makeVariableGraph(,sc)
+    plot(g)
+
+    g = makeTaskGraph(,sc)
+    plot(g)
 
 
 
-g2 = makeCallGraph("package:CodeDepends") # package with prefix
-g3 = makeCallGraph("getInputs") # single fun
-g4 = makeCallGraph(c("package:CodeDepends", "package:Rgraphviz")) #multiple pkgs
-g5 = makeCallGraph(c("package:CodeDepends", "edgeData")) #mix pkg and fun
+    g2 = makeCallGraph("package:CodeDepends") # package with prefix
+    g3 = makeCallGraph("getInputs") # single fun
+    g4 = makeCallGraph(c("package:CodeDepends", "package:Rgraphviz")) #multiple pkgs
+    g5 = makeCallGraph(c("package:CodeDepends", "edgeData")) #mix pkg and fun
+}
